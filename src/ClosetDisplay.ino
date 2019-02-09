58657a426b36eda1dc87b6bff9f9185dd9bbfdf1
Kevin McQuown <kevin@glancemirror.com>
2019-02-09 09:07:16 -0600

    correct for video

--- src/ClosetDisplay.ino
+++ src/ClosetDisplay.ino
@@ -4,6 +4,8 @@ SYSTEM_THREAD(ENABLED);
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
+#define TEMP_DATA_SIZE 10
+#define QUEUE_SIZE 10
 #define OLED_RESET D4
 #define TIME_STRING_SIZE 10
 #define DATA_IS_OLD 10000 // no update after these number of milliseconds
@@ -12,6 +14,30 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 unsigned long timeOfLastUpdate;
 char timeString[TIME_STRING_SIZE];
+
+os_thread_t thread;
+os_thread_t cloudThread;
+os_mutex_t mutex;
+os_queue_t queue;
+
+void sendToCloud(void *arg) {
+  char data[TEMP_DATA_SIZE];
+  while (1) {
+    os_queue_take(queue, &data, 100000, 0);
+    if (Particle.connected()) {
+      Particle.publish("temperature", data);
+    }
+  }
+}
+
+void messageLED(void *arg) {
+  while (1) {
+    os_mutex_lock(mutex);
+    digitalWrite(D5, HIGH);
+    delay(2000);
+    digitalWrite(D5, LOW);
+  }
+}
 
 void displayMessage(uint8_t textSize, uint8_t lineNumber, const char *data) {
   display.setTextSize(textSize);
@@ -22,16 +48,24 @@ void displayMessage(uint8_t textSize, uint8_t lineNumber, const char *data) {
 }
 void showTemp(const char *event, const char *data)
 {
+  char localCopy[TEMP_DATA_SIZE];
+  memcpy((void*)&localCopy, (void *)data, TEMP_DATA_SIZE);
+  os_queue_put(queue, localCopy, 0, 0);
   unsigned long delta = millis() - timeOfLastUpdate;
   timeOfLastUpdate = millis();
   display.clearDisplay();
-  displayMessage(2,0,data);
+  displayMessage(2,0,localCopy);
   snprintf(timeString, sizeof(timeString), "%d", delta);
   displayMessage(2, 1, timeString);
+  os_mutex_unlock(mutex);
+
 }
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  pinMode(D5, OUTPUT);
+  digitalWrite(D5, LOW);
 
   digitalWrite(ANTSW1, 0);
   digitalWrite(ANTSW2, 1);
@@ -42,6 +76,26 @@ void setup() {
   Mesh.on();
   Mesh.connect();
   waitUntil(Mesh.ready);
+
+  display.clearDisplay();
+  displayMessage(2, 0, "WiFi...");
+  WiFi.on();
+  WiFi.connect();
+  waitUntil(WiFi.ready);
+
+  display.clearDisplay();
+  displayMessage(2, 0, "Prtcl...");
+  Particle.connect();
+  waitUntil(Particle.connected);
+
+  display.clearDisplay();
+  displayMessage(2, 0, "Temp...");
+
+  os_mutex_create(&mutex);
+  os_queue_create(&queue, TEMP_DATA_SIZE, QUEUE_SIZE, NULL);
+
+  os_thread_create(&thread, NULL, OS_THREAD_PRIORITY_DEFAULT, messageLED, NULL, 2048);
+  os_thread_create(&cloudThread, NULL, OS_THREAD_PRIORITY_DEFAULT, sendToCloud, NULL, 2048);
 
   Mesh.subscribe("newTemp", showTemp);
 }
