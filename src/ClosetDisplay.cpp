4178ae01de8bc8664ef3e0f38b5d3557a6fcce7f
Kevin McQuown <kevin@glancemirror.com>
2019-03-22 11:31:34 -0500

    added watchdog and reason for reset

--- src/ClosetDisplay.cpp
+++ src/ClosetDisplay.cpp
@@ -15,17 +15,24 @@ SYSTEM_THREAD(ENABLED);
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
+#include "WCL_WatchDog.h"
+
 #define TEMP_DATA_SIZE 10
 #define MQTT_CLIENT_NAME "argonTempDisplay"
 #define QUEUE_SIZE 10
-#define OLED_RESET D4
+#define TRIGGER_WDT_BTN D3
+#define PETTING_OFF A1
 #define TIME_STRING_SIZE 10
 #define DATA_IS_OLD 10000 // no update after these number of milliseconds
 
+#define WATCHDOG_TIMEOUT_MS 30000
+
 void callback(char* topic, byte* payload, unsigned int length) {}
 
-MQTT client("test.mosquitto.org", 1883, callback);
-Adafruit_SSD1306 display(OLED_RESET);
+MQTT client("lab.thewcl.com", 1883, callback);
+Adafruit_SSD1306 display(D18);
+
+WCL_WatchDog wd;
 
 unsigned long timeOfLastUpdate;
 char timeString[TIME_STRING_SIZE];
@@ -38,7 +45,7 @@ os_queue_t queue;
 void sendToCloud(void *arg) {
   char data[TEMP_DATA_SIZE];
   while (1) {
-    os_queue_take(queue, &data, 100000, 0);
+    os_queue_take(queue, &data, CONCURRENT_WAIT_FOREVER, 0);
     if (client.isConnected()) {
       client.publish("wcl/ArgonDisplay", data);
     }
@@ -76,17 +83,28 @@ void showTemp(const char *event, const char *data)
   snprintf(timeString, sizeof(timeString), "%d", delta);
   displayMessage(2, 1, timeString);
   os_mutex_unlock(mutex);
-
 }
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
   pinMode(D5, OUTPUT);
+  pinMode(PETTING_OFF, OUTPUT);
   digitalWrite(D5, LOW);
+  digitalWrite(PETTING_OFF, LOW);
+
+  pinMode(TRIGGER_WDT_BTN, INPUT);
 
   digitalWrite(ANTSW1, 0);
   digitalWrite(ANTSW2, 1);
+
+  display.clearDisplay();
+  if (System.resetReason() == RESET_REASON_WATCHDOG) {
+    displayMessage(2, 0, "WDT RST");
+  } else {
+    displayMessage(2, 0, "RST");
+  }
+  delay(2000);
 
   display.clearDisplay();
   displayMessage(2, 0, "Mesh...");
@@ -109,12 +127,6 @@ void setup() {
   display.clearDisplay();
   displayMessage(2, 0, "Temp...");
 
-  client.connect(MQTT_CLIENT_NAME);
-
-  if (client.isConnected()) {
-      client.publish("wcl/ArgonDisplay","hello world!");
-  }
-
   os_mutex_create(&mutex);
   os_queue_create(&queue, TEMP_DATA_SIZE, QUEUE_SIZE, NULL);
 
@@ -122,10 +134,20 @@ void setup() {
   os_thread_create(&cloudThread, NULL, OS_THREAD_PRIORITY_DEFAULT, sendToCloud, NULL, 2048);
 
   Mesh.subscribe("newTemp", showTemp);
+
+  wd.initialize(WATCHDOG_TIMEOUT_MS);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
+bool keepPettingWatchDog = true;
+
 void loop() {
+  if (digitalRead(TRIGGER_WDT_BTN) == LOW) {
+    keepPettingWatchDog = false;
+    digitalWrite(PETTING_OFF, HIGH);
+  }
+  if (keepPettingWatchDog && Particle.connected()) {
+    wd.pet();
+  }
   unsigned long delta = millis() - timeOfLastUpdate;
   if (delta > DATA_IS_OLD) {
       display.setTextSize(2);
